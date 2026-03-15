0b14dc060908ef1e50bd27337d3293983b7c9385
AbrorShukurov <76687825+AbrorShukurov@users.noreply.github.com>
2026-03-16 03:23:04 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,39 +20,58 @@ import BlogPage from "./pages/BlogPage";
 import BlogArticlePage from "./pages/BlogArticlePage";
 
 function App() {
-  return (
-    <div className="App">
-      <LanguageProvider>
-        <BrowserRouter>
-          <Navbar />
-          <main>
-            <Routes>
-              {/* Main Pages */}
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/contact" element={<Contact />} />
-              
-              {/* Service Pages */}
-              <Route path="/xizmatlar/:slug" element={<ServicePage />} />
-              
-              {/* Pricing Page */}
-              <Route path="/narxlar" element={<PricingPage />} />
-              
-              {/* Blog Pages */}
-              <Route path="/blog" element={<BlogPage />} />
-              <Route path="/blog/:slug" element={<BlogArticlePage />} />
-              
-              {/* Boiler Brand Pages */}
-              <Route path="/katyollar/:slug" element={<BoilerBrandPage />} />
-            </Routes>
-          </main>
-          <Footer />
-          <Toaster position="top-center" richColors />
-        </BrowserRouter>
-      </LanguageProvider>
-    </div>
-  );
+return ( <div className="App"> <LanguageProvider> <BrowserRouter> <Navbar /> <main> <Routes>
+
+```
+          {/* Uzbek Pages */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+
+          {/* Service Pages */}
+          <Route path="/xizmatlar/:slug" element={<ServicePage />} />
+
+          {/* Pricing */}
+          <Route path="/narxlar" element={<PricingPage />} />
+
+          {/* Blog */}
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/:slug" element={<BlogArticlePage />} />
+
+          {/* Boiler Brand */}
+          <Route path="/katyollar/:slug" element={<BoilerBrandPage />} />
+
+
+          {/* Russian Pages */}
+          <Route path="/ru" element={<Home />} />
+          <Route path="/ru/about" element={<About />} />
+          <Route path="/ru/services" element={<Services />} />
+          <Route path="/ru/contact" element={<Contact />} />
+
+          {/* Russian Service Pages */}
+          <Route path="/ru/xizmatlar/:slug" element={<ServicePage />} />
+
+          {/* Russian Pricing */}
+          <Route path="/ru/narxlar" element={<PricingPage />} />
+
+          {/* Russian Blog */}
+          <Route path="/ru/blog" element={<BlogPage />} />
+          <Route path="/ru/blog/:slug" element={<BlogArticlePage />} />
+
+          {/* Russian Boiler Brand */}
+          <Route path="/ru/katyollar/:slug" element={<BoilerBrandPage />} />
+
+        </Routes>
+      </main>
+      <Footer />
+      <Toaster position="top-center" richColors />
+    </BrowserRouter>
+  </LanguageProvider>
+</div>
+```
+
+);
 }
 
 export default App;
